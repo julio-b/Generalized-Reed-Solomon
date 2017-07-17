e97b4de671b166e4f53fdb569b17f80d1ba495f5
Julio B <julio.bacel@gmail.com>
2017-07-17 15:34:03 +0300

    Add error highlighting

--- client.py
+++ client.py
@@ -6,7 +6,6 @@ import genreedsolomon as grs
 import argparse
 
 def main(argv):
-    #TODO color msg errors, --no-color
     args = parseArguments(argv)
     print args
     HOST = "localhost"
@@ -25,13 +24,21 @@ def main(argv):
         print "Received", len(data[1]), "messages from server!\n"
         for i in range(len(data[1])):
             try:
-                if(args.to_code):
-                    di = D.decode_to_code(data[1][i])
-                else:
-                    di = D.decode_to_message(data[1][i])
+                if(args.no_color):
+                    strcode = str(data[1][i])
+                else: #highlight errors
+                    strcode = "("
+                    code = D.decode_to_code(data[1][i])
+                    for c in range(len(data[1][i])):
+                        if(code[c]!=data[1][i][c]):
+                            strcode += "\033[6;30;41m" + str(data[1][i][c]) + "\033[0m"
+                        else:
+                            strcode += str(data[1][i][c])
+                        strcode += ", "
+                    strcode += "\b\b)"  #*, ) --> *)
                 if(args.to_string):
                     strmsg += grs.decodeToAscii(data[1][i], C, D)
-                print "#%d {%s --decode-to-%s--> %s}" % (i, data[1][i], "code" if args.to_code else "msg", di)
+                print "#%d {%s --decode-to-%s--> %s}" % (i, strcode, "code" if args.to_code else "msg", D.decode_to_code(data[1][i]) if args.to_code else D.decode_to_message(data[1][i]))
             except (sage.coding.decoder.DecodingError, ValueError) as e:
                 print "#%d {%s DECODE FAILED!}" % (i, data[1][i])
                 pass
@@ -55,6 +62,7 @@ def parseArguments(args):
     parser.add_argument("-P", "--port", default=666,type=int, dest="PORT", help="Socket port (default: 666)")
     parser.add_argument("-c", "--to-code", action="store_true", help="Decode to code instead of message")
     parser.add_argument("-s", "--to-string", action="store_true", help="Decode also to ascii string")
+    parser.add_argument("--no-color", action="store_true", help="Do not highlight errors in code")
     return parser.parse_args(args)
 
 if __name__ == "__main__":
